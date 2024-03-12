309528a4b9673aeb9e17d1517dd297c047165627
herosax <162419144+herosax@users.noreply.github.com>
2024-03-12 08:10:32 -0700

    Update index.php

--- index.php
+++ index.php
@@ -66,69 +66,61 @@ function batt($url, $method, $data = null) {
 }
 
 
-function curl($url, $method, $data = null) {
-	global $vvv;
-    $header = array(     
-        "X-Forwarded-For: 188.74.210.207",
-        "Content-Type: application/json",
-        "User-Agent: ".$vvv.""
-    );
-    $proxy = 'socks5://temera88:temera88@188.74.210.207:6286';
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($ch, CURLOPT_COOKIE, "");
-    curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
-    curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
-    curl_setopt($ch, CURLOPT_PROXY, $proxy);       
-    curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
-    if ($method === 'POST') {
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+function solveCaptcha(){
+	global $vvv, $sit;
+a:
+$login = "http://sctg.xyz/in.php?key=sryKi9OUWF4Rfrl7w7oplqmk5QhRWosR&method=userrecaptcha&googlekey=".$sit."&json=1&pageurl=https://coins-battle.com/game/claimreward";
+$ua[] = "User-Agent: ".$vvv."";
+$ua[] = "Content-Type: application/json";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $login);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $ua);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+$result = curl_exec($ch);
+
+$re = json_decode($result);
+$id = $re->request;
+if($id==''){goto a;}
+c:
+$url = "http://sctg.xyz/res.php?key=sryKi9OUWF4Rfrl7w7oplqmk5QhRWosR&action=get&id=".$id."";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $ua);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+$proxy = 'socks5://ksnpkrah:rc5efxlxj6o4@188.74.210.207:6286';
+curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);       
+curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
+$res = curl_exec($ch);
+
+if ($res == 'CAPCHA_NOT_READY') {          
+        sleep(6);
+        goto c;
     }
-    $result = curl_exec($ch);
-    curl_close($ch);
-    return $result;
+if($res=="ERROR_CAPTCHA_UNSOLVABLE"){sleep(80);goto a;}
+$captcha = str_replace("OK|", "", $res);
+curl_close($ch);
+return $captcha;
 }
-function cap($url, $method, $data = null) {
-	global $vvv;
-    $header = array(        
-        
-        "Content-Type: application/json",
-        "User-Agent: ".$vvv.""
-    );
-    
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($ch, CURLOPT_COOKIE, "");
-      
-    if ($method === 'POST') {
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    }
-    $result = curl_exec($ch);
-    curl_close($ch);
-    return $result;
-}
-$hh="621";
+
+
+
+$hh="634";
 xx:
-if($hh=="631"){echo "Complete!!! \n";sleep(99999);}
+if($hh=="651"){echo "Complete!!! \n";sleep(99999);}
 //$url = "https://excentiv.com/offerwall/?userid=43981&key=kMb1m7Rirq8Hpta06GcU";
-$url = "https://excentiv.com/offerwall/?userid=798143eb1aaa&key=5eaQHDSVYcwbdACp6ZB7";
+$url = "https://excentiv.com/offerwall/?userid=c6104ac5b5a1&key=5eaQHDSVYcwbdACp6ZB7";
 $of = ofer($url, 'GET');
 
 sleep(5);
+//if (strpos($of, "Games") === false) {echo" Game Hilang \n";sleep(99999);}
 $tokk = explode('"',explode('<button value="https://coins-battle.com?token=', $of)[1])[0];
 if($tokk==""){sleep(3);goto xx;}
+
 $url = "https://coins-battle.com/?token=".$tokk."";
 $bat = batt($url, 'GET');
 
@@ -143,36 +135,10 @@ $tim = explode("';",explode("let ctimer = '", $btc)[1])[0];
 $lef = explode(' today',explode('<p><b>You have already play ', $btc)[1])[0];
 if($idd==""){goto xx;}
 
-a:
-$url = "http://sctg.xyz/in.php?";
-$data = array(
-    'key' => '3AZ9DG7S3JvRITp5qaovctLQZIesH19w',
-    'method' => 'userrecaptcha',
-    'googlekey' => $sit,
-    'json' => '1',
-    'pageurl' => 'https://coins-battle.com/game/claimreward'
-);
-$datax = http_build_query($data);
-$respo = cap($url, 'POST', $datax);
-$re = json_decode($respo);
-$id = $re->request;
-if($id==''){goto a;}
-$r = 1;
-
-c:
-$url = "http://sctg.xyz/res.php?key=3AZ9DG7S3JvRITp5qaovctLQZIesH19w&action=get&id=".$id."";
-$res = curl($url, 'GET');
-if ($res == 'CAPCHA_NOT_READY') {          
-        sleep(6);
-        goto c;
-    }
-if($res=="ERROR_CAPTCHA_UNSOLVABLE"){sleep(80);goto a;}
-
-$captcha = str_replace("OK|", "", $res);
-
+$capv = solveCaptcha();
 
 $url = 'https://coins-battle.com/game/claimreward';
-$data = "game_id=".$idd."&csrf_token=".$csf."&captcha=recaptchav2&g-recaptcha-response=".$captcha."";
+$data = "game_id=".$idd."&csrf_token=".$csf."&captcha=recaptchav2&g-recaptcha-response=".$capv."";
 $las = batt($url, 'POST', $data);
 
 $suc = explode(', to continue earning',explode('<div class="alert text-center alert-success"><i class="fa fa-check-circle"></i> ', $las)[1])[0];
@@ -180,6 +146,7 @@ date_default_timezone_set('Asia/Jakarta');
 $timestamp = time();
 $wak = date("[H:i]", $timestamp);
 if (strpos($suc, "obtained") !== false) {echo"".$wak." ID [".$hh."] $suc \n";}
+
 sleep($tim);
 if($lef=="29/30"){$hh=$hh+1;goto xx;}
 endwhile;
